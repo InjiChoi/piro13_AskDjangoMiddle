9959656ac68374319866e50c30aeaca9e8c9ddbd
InjiChoi <chldlswl1212@gmail.com>
2020-07-28 02:09:28 +0900

    07 회원가입과 동시에 로그인하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+from .forms import SignupForm
 
 # Create your views here.
 @login_required
@@ -11,20 +13,30 @@ def profile(request):
     return render(request, 'accounts/profile.html')
 
 # 회원가입 첫번째 방법
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect(settings.LOGIN_URL)
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'accounts/signup.html', {
-#         'form':form,
-#     })
+'''def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user) #로그인 처리
+            return redirect('profile')
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {
+        'form':form,
+    })'''
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect('profile')
 
 # 회원가입 두번째 방법
-signup = CreateView.as_view(model=User, 
-        template_name='accounts/signup.html',
-        success_url = settings.LOGIN_URL,
-        form_class=UserCreationForm)
+signup = SignupView.as_view()
